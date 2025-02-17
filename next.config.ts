5be7117e6901af2090c293ffcecbf3dbc6c97b46
i001962@gmail.com <i001962@gmail.com>
2025-02-16 19:38:38 -0800

    fhub next config update

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "**",
-      }, 
+      },
     ],
   },
-  
-
+  webpack: (config, { isServer }) => {
+    // Only apply these fallbacks on the client-side.
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        "@connectrpc/connect-node": false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
