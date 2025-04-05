079fb8c3d4384d30ec780de0a026f622c6a72117
i001962@gmail.com <i001962@gmail.com>
2025-04-04 20:56:36 -0700

    adds cast action insted of intent

--- src/components/ui/WarpcastShareButton.tsx
+++ src/components/ui/WarpcastShareButton.tsx
@@ -211,6 +211,10 @@ export function WarpcastShareButton({ selectedMatch, buttonText, compositeImage 
         window.open(url, '_blank');
       } else {
         frameSdk.actions.openUrl(url);
+        await frameSdk.actions.composeCast({ 
+          encodedSummary,
+          encodedMiniAppUrl,
+        })
       }
     }
   }, [selectedMatch, context, searchParams, compositeImage]);
